dc6143d5c7aac859a0cba9178f52b2804abfac2d
Bhargav Siripurapu <104213005+bargavsiripurapu@users.noreply.github.com>
2022-11-21 15:09:31 +0530

    code committed for EBS volume

--- demo-9/vars.tf
+++ demo-9/vars.tf
@@ -1,5 +1,5 @@
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "us-east-1"
 }
 
 variable "PATH_TO_PRIVATE_KEY" {
@@ -11,11 +11,6 @@ variable "PATH_TO_PUBLIC_KEY" {
 }
 
 variable "AMIS" {
-  type = map(string)
-  default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-844e0bf7"
-  }
+  default = "ami-need to update ami from aws"
 }
 
